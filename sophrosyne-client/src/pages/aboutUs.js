56f73172df1ba4305dea1ba8a1855db7e7db2e0c
Mohandas, Swetha <smohandas3@gatech.edu>
2020-07-09 15:40:25 -0700

    about us fixed

--- sophrosyne-client/src/pages/aboutUs.js
+++ sophrosyne-client/src/pages/aboutUs.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import { Image } from 'semantic-ui-react';
+import map from './4euni.png'
 import './aboutUs.css'
  
 import {Link} from 'react-router-dom'
@@ -15,6 +17,7 @@ class aboutUs extends Component {
             <div className = 'aboutUs' >
                 <h1 style = {aboutStyle}>About Us</h1>
                 <p style = {textStyle}>Here at TheresHope, we work hard to <br></br>create connections, whilst also providing<br></br>outlets of professional help.</p>
+                <Image alt = 'map' src = { map } />
             </div>
         )
     }
