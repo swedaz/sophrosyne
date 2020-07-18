d90b3fa6d7b351df47a1244150a2f37be780367f
Mohandas, Swetha <smohandas3@gatech.edu>
2020-07-18 15:44:06 -0700

    about us updated

--- sophrosyne-client/src/pages/aboutUs.js
+++ sophrosyne-client/src/pages/aboutUs.js
@@ -4,21 +4,32 @@ import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import { Image } from 'semantic-ui-react';
 import map from './4euni.png'
+import team from './team2.png'
 import './aboutUs.css'
  
 import {Link} from 'react-router-dom'
 
 const aboutStyle = {color: 'white', fontSize: '105px', fontFamily: 'Arial', display: 'flex', alignItems: 'center', textAlign: 'center'};
 const textStyle = {color: 'white', fontSize: '42px', fontFamily: 'Arial', display: 'flex', alignItems: 'center'};
+
 class aboutUs extends Component {
     
     render() {
         return (
-            <div className = 'aboutUs' >
-                <h1 style = {aboutStyle}>About Us</h1>
-                <p style = {textStyle}>Here at TheresHope, we work hard to <br></br>create connections, whilst also providing<br></br>outlets of professional help.</p>
-                <Image alt = 'map' src = { map } />
-            </div>
+            <div>
+                <div className = 'aboutUs' >
+                    <h1 style = {aboutStyle}>About Us</h1>
+                    <p style = {textStyle}>Here at TheresHope, we work hard to <br></br>create connections, whilst also providing<br></br>outlets of professional help.</p>
+                    <Image alt = 'map' src = { map } />
+                </div>
+                <div className = 'abouUs2'>
+                    <h1 style = {aboutStyle}>About Us</h1>
+                    <p style = {textStyle}>Here at TheresHope, we work hard to <br></br>create connections, whilst also providing<br></br>outlets of professional help.</p>
+                </div>
+                <div className = 'abouUs3'>
+                    <Image className = 'team' alt = 'team' src = { team }/>
+                </div>
+            </div>
         )
     }
 }
