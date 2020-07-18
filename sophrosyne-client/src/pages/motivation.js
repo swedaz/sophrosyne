437f8df63c909c9abe140d3e1cd9d732c381b262
Mohandas, Swetha <smohandas3@gatech.edu>
2020-07-18 15:54:27 -0700

    motivation page and button

--- sophrosyne-client/src/pages/motivation.js
+++ sophrosyne-client/src/pages/motivation.js
@@ -4,24 +4,24 @@ import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import { Image } from 'semantic-ui-react';
 import Grid from '@material-ui/core/Grid';
+import Profile from '../components/Profile'
+
  
 import {Link} from 'react-router-dom'
 
 const aboutStyle = {color: 'black', fontSize: '105px', fontFamily: 'Arial', display: 'flex', alignItems: 'center', textAlign: 'center'};
 
 
-class help extends Component {
+class motivation extends Component {
     
     render() {
         return (
-            <div className = 'help' >
-                <h1 style= {{aboutStyle, marginTop: '50px'}}>HELPPPP</h1>
-                <h1 style= {{aboutStyle}}>MOTIVATION</h1>
-                <h1 style= {{aboutStyle}}>MOTIVATION</h1>
-
+            <div className = 'motivation' >
+                <Profile />
+                <h1 style = {aboutStyle}>motivation</h1>
             </div >
         )
     }
 }
 
-export default help
+export default motivation
